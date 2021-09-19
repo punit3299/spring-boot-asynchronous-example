60e535cacdbca1463de4bc0ad37f65743a547ef1
Punit Raj <raj.punit3299@gmail.com>
2021-09-19 19:41:27 +0530

    project init

---
+++ src/main/java/com/asynchronous/SpringBootAsynchronousExampleApplication.java
@@ -0,0 +1,13 @@
+package com.asynchronous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAsynchronousExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAsynchronousExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asynchronous/SpringBootAsynchronousExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asynchronous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAsynchronousExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
